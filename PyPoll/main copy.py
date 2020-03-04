f13d0b92f9d82b4418d33c0bf7a3ebc28e0e64a5
kem424 <60161315+kem424@users.noreply.github.com>
2020-03-04 00:06:18 -0500

    Update main copy.py

--- PyPoll/main copy.py
+++ PyPoll/main copy.py
@@ -3,81 +3,65 @@ import os
 import csv
 
 #open CSV file
-file=os.path.join("election_data.csv")
+file=os.path.join('Resources','election_data.csv')
 with open (file, 'r', newline= "") as csvfile:
-    reader=csv.reader(csvfile, delimiter= ",")
-    #skip header
-    next(reader)
-
-    #Iteration
-    total_votes= 0
-    percent_of_votes= 0.0
-    winner_votes= 0
-    winner=""
+    csvreader=csv.reader(csvfile, delimiter= ",")
    
-    khan_total= 0
-    khan_percentage= 0.00
-    correy_total=0
-    correy_percentage= 0.00
-    li_total= 0
-    li_percent= 0.00
-    o_tooley_total=0
-    o_tooley_percent= 0.00
-    candidates= []
 
-    candidates=row[2]
+   #variables
+total_votes = 0
+candidates_unique = []
+candidate_votes = []
 
-#if candidate has other votes then add to vote tally
-if candidate in candidates:
-    candidate_index = candidates.index(candidate)
-    vote_counts[candidate_index] = vote_counts[candidate_index] + 1
- #else create new spot in list for candidate
-else:
-    candidates.append(candidate)
-    vote_counts.append(1)
-
-
-    #loop the data
-    #total_votes=len(candidate)
-    for row in csvreader:
+for row in csvreader:
+        #Total Votes
         total_votes += 1
+        #skip 
+        candidate_in = (row[2])
+        #add to vote count if candidate already exists
+        if candidate_in in candidates_unique:
+            candidate_index = candidates_unique.index(candidate_in)
+            candidate_votes[candidate_index] = candidate_votes[candidate_index] + 1
+        else:
+            #if candidate was not found in candidates list then append to list and add 1 to vote count
+            candidates_unique.append(candidate_in)
+            candidate_votes.append(1)
 
+pct = []
+max_votes = candidate_votes[0]
+max_index = 0
+
+for x in range(len(candidates_unique)):
+    #calculation to get the percentage
+    vote_pct = round(candidate_votes[x]/total_votes*100, 2)
+    pct.append(vote_pct)
     
-    for row in candidate_list:
-        if row[1]== winner:
-            winner_name= row[0]
+    if candidate_votes[x] > max_votes:
+        max_votes = candidate_votes[x]
+        max_index = x
+
+election_winner = candidates_unique[max_index] 
 
 
-
-        khan_total= candidate.count('Khan')
-        khan_percent=int(khan_total)/int(total_votes)*100
-
-        correy_total=candidate.count('Correy')
-        correy_percent= (correy_total/total_votes)*100
-
-        li_total=candidate.count('Li')
-        li_percent= (li_total/total_votes)*100
-
-        o_tooley_total=candidate.count("O'Tooley")
-        o_tooley_percent= (o_tooley_total/total_votes)*100
-
-        #write the results to a dictionary array
-        candidate_totals_percentage=[khan_percent,correy_percent,li_percent,o_tooley_percent]
-
-        #print "max(candidates_totals_percentage) to calculate winner"
-        winner=max(candidates_totals_percentage)
-        
+print('|                  Election Results                  |')
+print('======================================================')
+print(f'Total Votes: {total_votes}')
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+for x in range(len(candidates_unique)):
+    print(f'{candidates_unique[x]} : {pct[x]}% ({candidate_votes[x]})')
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print(f'Election winner: {election_winner.upper()}')
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 
-    
-
-print("Election Results")
-print("_________________")
-print("Total:Votes:"+ str(total_votes))
-print("_________________________")
-print(f"Khan:{str(Khan_percent[0])}({str(khan_total)})")
-print(f"Correy:{str(Correy_percent[0])}({str(Correy_total)})")
-print(f"Li:{str(Li_percent[0])}({str(Li_total)})")
-print(f"O'Tooley:{str(o_tooley_percent[0])}({str(o_tooley_total)})")
-print("__________________________")
-print("Winner:"+str(winner))
+Election Results
+-------------------------
+Total Votes: 3521001
+-------------------------
+Khan: 63.000% (2218231)
+Correy: 20.000% (704200)
+Li: 14.000% (492940)
+O'Tooley: 3.000% (105630)
+-------------------------
+Winner: Khan
+-------------------------
